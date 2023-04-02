a9233d4dae9c936e4db7087747b3aa40f3be4f61
Yzee <yan.zhigurs@mail.ru>
2023-04-02 11:25:16 +0500

    Initial Commit

---
+++ AppleMusicSwiftUI/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ян Жигурс on 02.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
