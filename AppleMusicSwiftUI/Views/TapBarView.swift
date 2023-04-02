c92ea1b0298bc1cba54c25d964c29464783a6181
Yzee <yan.zhigurs@mail.ru>
2023-04-02 11:46:31 +0500

    TapBarView

---
+++ AppleMusicSwiftUI/Views/TapBarView.swift
@@ -0,0 +1,40 @@
+//
+//  TapBarView.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ян Жигурс on 02.04.2023.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            LiberyView()
+                .tabItem {
+                    Image(systemName: "rectangle.stack.badge.play")
+                    Text("Медиатека")
+                }
+            
+            Text("Радио")
+                .tabItem {
+                    Image(systemName: "dot.radiowaves.left.and.right")
+                    Text("Радио")
+                }
+            
+            Text("Поиск")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Поиск")
+                }
+            
+        }
+        .accentColor(.red)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
